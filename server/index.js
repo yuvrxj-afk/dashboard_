8e22ca51d4a8e953d9347daf999f705f289a2c2e
uv-07 <63532070+uv-07@users.noreply.github.com>
2023-03-17 14:40:48 +0530

    client/server

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ import dotenv from 'dotenv'
 import helmet from "helmet";
 import morgan from 'morgan'
 import clientRoutes from './routes/client.js'
-import generalRoutes from './routes/general.js'
-import managementRoutes from './routes/management.js'
-import salesRoutes from './routes/sales.js'
+// import generalRoutes from './routes/general.js'
+// import managementRoutes from './routes/management.js'
+// import salesRoutes from './routes/sales.js'
 
 
 /* configuration */
@@ -28,6 +28,19 @@ app.use(cors())
 /* Routes */
 
 app.use('/client', clientRoutes)
-app.use('/general', generalRoutes)
-app.use('/management', managementRoutes)
-app.use('/sales', salesRoutes)
+// app.use('/general', generalRoutes)
+// app.use('/management', managementRoutes)
+// app.use('/sales', salesRoutes)
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server Port : ${PORT}`);
+    })
+}).catch((err) => {
+    console.log(`${err} did not connect`);
+})
